340aebb0c19e4ba39f0c70f788f7ab07cacda03a
Elringus <elringus@yandex.ru>
2014-10-03 18:16:03 +0400

    add auth

--- main.py
+++ main.py
@@ -14,34 +14,43 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write('Hello World, this is the starcor server!')
 
 
+class Auth(webapp2.RequestHandler):
+    def get(self):
+        player = Player.query(Player.login == self.request.get('login')).get()
+        data = json.dumps({'Auth': 'OK' if player is not None else 'FAIL'})
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(data)
+
+
 class Level(webapp2.RequestHandler):
     def get(self):
-        player = Player.query(Player.login == self.request.get('login')).fetch(1)[0]
+        player = Player.query(Player.login == self.request.get('login')).get()
         data = json.dumps({'Level': player.level})
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
 
     def post(self):
-        player = Player.query(Player.login == self.request.get('login')).fetch(1)[0]
+        player = Player.query(Player.login == self.request.get('login')).get()
         player.level = int(self.request.get('level'))
         player.put()
 
 
 class Exp(webapp2.RequestHandler):
     def get(self):
-        player = Player.query(Player.login == self.request.get('login')).fetch(1)[0]
+        player = Player.query(Player.login == self.request.get('login')).get()
         data = json.dumps({'Exp': player.exp})
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
 
     def post(self):
-        player = Player.query(Player.login == self.request.get('login')).fetch(1)[0]
+        player = Player.query(Player.login == self.request.get('login')).get()
         player.exp = float(self.request.get('exp'))
         player.put()
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/auth', Auth),
     ('/level', Level),
     ('/exp', Exp)
 ], debug=True)
